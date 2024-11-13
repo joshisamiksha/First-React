b23b5ffbd0fcd4d2ef99c14252657dece9a621ff
samiksha <samiksha.10708856@ltimindtree.com>
2024-11-13 16:47:59 +0530

    second

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Basic from "./basic.js";
+
+function App() {
+  return (
+    <>
+      <Basic />
+      <h1>Basic React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/basic.js
@@ -0,0 +1,7 @@
+function Basic() {
+    return(
+        <h2>This is a basic project</h2>
+    )
+}
+
+export default Basic
