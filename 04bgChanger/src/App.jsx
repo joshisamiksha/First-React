4c19d44daba5127bfccc925b526cde8a57a5869c
samiksha <samiksha.10708856@ltimindtree.com>
2024-11-16 15:04:04 +0530

    bgChanger

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("darkgray")
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}></div>
+    <div className="fixed flex flex-wrap justify-center bottom-14 inset-x-0 px-2">
+      <div className="fixed flex flex-wrap justify-center gap-3 shadow-lg bg-slate-50 px-3 py-2 rounded-3xl">
+          <button onClick={() => setColor("red")} className="rounded-sm px-4 shadow-lg" style={{ backgroundColor: "Red"}}>Red</button>
+          <button onClick={() => setColor("Green")} className="rounded-sm px-4" style={{ backgroundColor: "Green" }}>Green</button>
+          <button onClick={() => setColor("Yellow")} className="rounded-sm px-4" style={{ backgroundColor: "Yellow" }}>Yellow</button>
+          <button onClick={() => setColor("Blue")} className="rounded-sm px-4" style={{ backgroundColor: "Blue" }}>Blue</button>
+          <button onClick={() => setColor("Orange")} className="rounded-sm px-4" style={{ backgroundColor: "Orange" }}>Orange</button>
+          <button onClick={() => setColor("Pink")} className="rounded-sm px-4" style={{ backgroundColor: "Pink" }}>Pink</button>
+          <button onClick={() => setColor("Gray")}  className="rounded-sm px-4" style={{ backgroundColor: "Gray" }}>Gray</button>
+          <button onClick={() => setColor("Purple")} className="rounded-sm px-4" style={{ backgroundColor: "purple" }}>purple</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
